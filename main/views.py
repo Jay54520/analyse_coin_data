1a89986eece50d906c96a4cb09711d0ba0052da0
揭思敏 <jsm0834@175game.com>
2018-05-06 13:59:39 +0800

    展示 coin_ids

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
+import json
+import requests
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def coin_ids(request):
+    response = requests.get('http://127.0.0.1:8888/coin_ids')
+    _coin_ids = json.loads(response.content)
+    th_list = ['coin_id']
+    tr_list = [[coin_id] for coin_id in _coin_ids]
+    context = {
+        'th_list': th_list,
+        'tr_list': tr_list,
+    }
+    return render(request, "main/display_coin_ids.html", context=context)
